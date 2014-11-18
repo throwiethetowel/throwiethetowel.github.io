e81daad7a1174eb431255b225830f3da0f2f9811
throwiethetowel <dblspc@gmail.com>
2014-11-18 14:37:18 -0700

    Update kdp-plus.js

--- static/kdp-plus.js
+++ static/kdp-plus.js
@@ -169,5 +169,5 @@ if (!Array.prototype.reduce) {
     function dateToString(date) {
         return [date.getFullYear(), date.getMonth()+1, date.getDate()].join("-");
     }
-interval = setInterval(renderSalesWidget, 300000);
+interval = setInterval(renderSalesWidget, 100000);
 })();
