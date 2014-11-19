3cdfc6674d435d0f3acf6c2f8ab4dbf4fafb3d39
throwiethetowel <dblspc@gmail.com>
2014-11-18 17:54:54 -0700

    Create bignumber.js

---
+++ bignumber.js
@@ -0,0 +1,76 @@
+$('head').append('<link rel=\"stylesheet\" type=\"text/css\" href=\"https://rosenau.neocities.org/bignumbercss.css\">');
+$('body').html('<table id=\"td\"><tr><td></td></tr></table>');
+
+var intervalInMinutes = 1;
+var borrowRate = 1.33;
+
+function fetch() {
+	var custID = init.toString().substr(init.toString().indexOf('customerID')+14).split('\"')[0];
+	var today = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate();
+	$.ajax({
+		url: 'https://kdp.amazon.com/reports/data',
+		data: {
+			customerID: init.toString().substr(init.toString().indexOf("customerID")+14).split('\"')[0],
+			sessionID: "sessionK",
+			type: "OBR",
+			marketplaces: "all",
+			asins: '',
+			startDate: today,
+			endDate: today
+		},
+		success: function(data) {
+			data = JSON.parse(data);
+			var dollars = aaDataToUSD(data.aaData, data.borrowData[0]);
+			var str = '$' + Number(dollars).toFixed(2);
+      console.log(str);
+			$('td').html(str);
+		},
+		error: function(err) {
+			console.log('Error: ' + err);
+		}
+	});
+}
+
+function aaDataToUSD(aaData, borrowCount) {
+	dollars = 0;
+	for (var y = 0; y < aaData.length; y += 1) {
+		switch (aaData[y][2]) {
+			case 'USD':
+				dollars += Number(aaData[y][1]);
+				break;
+			case 'GBP':
+				dollars += Number(aaData[y][1])*1.6;
+				break;
+			case 'EUR':
+				dollars += Number(aaData[y][1])*1.25;
+				break;
+			case 'JPY':
+				dollars += Number(aaData[y][1])*0.0088;
+				break;
+			case 'INR':
+				dollars += Number(aaData[y][1])*0.016;
+				break;
+			case 'CAD':
+				dollars += Number(aaData[y][1])*0.88;
+				break;
+			case 'BRL':
+				dollars += Number(aaData[y][1])*0.4;
+				break;
+			case 'MXN':
+				dollars += Number(aaData[y][1])*0.074;
+				break;
+			case 'AUD':
+				dollars += Number(aaData[y][1])*0.87;
+				break;
+		}
+	}
+	dollars += borrowRate*borrowCount;
+	return dollars;
+}
+
+function sizeText() {
+    
+};
+
+fetch();
+setInterval(fetch, intervalInMinutes*60*1000);
